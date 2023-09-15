04e562ff0e77f2955495f8fd38c1a16fb417ff7f
strickland2k <67063735+strickland2k@users.noreply.github.com>
2023-09-15 14:36:38 -0400

    Update cypress.config.js

--- ui-test/cypress.config.js
+++ ui-test/cypress.config.js
@@ -11,29 +11,4 @@ module.exports = defineConfig({
   },
 
 
-
-  setupNodeEvents(on, config) {
-    registerDataSession(on, config)
-    require('@cypress/grep/src/plugin')(config)
-    return config
-  },
-
-  e2e: {
-    baseUrl: "https://admin.qa.music.onepeloton.com/",
-    trashAssetsBeforeRuns: false,
-    screenshotOnRunFailure: true,
-
-
-    "video": false,
-      "retries":{
-        "runMode": 1,
-        "openMode": 1
-      },
-
-    setupNodeEvents(on, config) {
-      registerDataSession(on, config)
-      require('@cypress/grep/src/plugin')(config)
-      return config
-    },
-  },
 });
