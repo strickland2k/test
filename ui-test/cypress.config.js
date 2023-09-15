fb498bcd701ff159e8a3995d65e6690f31f3b776
strickland2k <67063735+strickland2k@users.noreply.github.com>
2023-09-15 14:38:22 -0400

    Update cypress.config.js

--- ui-test/cypress.config.js
+++ ui-test/cypress.config.js
@@ -3,12 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 786,
   viewportWidth: 1024,
+
   // screenshotsFolder: "cypress/results/assets",
 
   reporter: "mocha-multi-reporters",
+
   reporterOptions: {
     mochaFile: "reporter-config.json",
   },
 
-
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
