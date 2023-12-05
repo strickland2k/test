7982ee8aaed4137d5eb290ee488f01b0f69a53ab
strickland2k <67063735+strickland2k@users.noreply.github.com>
2023-12-05 11:36:07 -0500

    maybe now

--- ui-test/mergeScreenshots.sh
+++ ui-test/mergeScreenshots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define the paths
-videosFolder="cypress/screenshots/"          # The folder where Cypress saves video recordings
-commonVideosFolder="public/screenshots/"  # The common folder where you want to move the videos
+videosFolder="/home/runner/work/test/test/ui-test/cypress/screenshots/"          # The folder where Cypress saves video recordings
+commonVideosFolder="/home/runner/work/test/test/ui-test/public/screenshots/"  # The common folder where you want to move the videos
 
 # Ensure the common folder exists
 mkdir -p "$commonVideosFolder"
