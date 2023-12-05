0643074ed567224b69ef8ccf23a6cf723ddea465
strickland2k <67063735+strickland2k@users.noreply.github.com>
2023-12-05 09:55:13 -0500

    maybe now

---
+++ ui-test/mergeScreenshots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the paths
+videosFolder="cypress/screenshots"          # The folder where Cypress saves video recordings
+commonVideosFolder="public/screenshots"  # The common folder where you want to move the videos
+
+# Ensure the common folder exists
+mkdir -p "$commonVideosFolder"
+
+# Move video files from subfolders to the common folder
+find "$videosFolder" -type f -name "* (failed) (attempt 2).png" -exec mv {} "$commonVideosFolder" \;
+
+
+echo "Picture files moved to the common folder."
